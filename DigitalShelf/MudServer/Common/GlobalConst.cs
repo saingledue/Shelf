c630f836d9a475c2438327a92c64a654c682bc4f
saingledue <saingledue@outlook.com>
2024-03-18 09:25:43 -0500

    pushing MidTerm

---
+++ DigitalShelf/MudServer/Common/GlobalConst.cs
@@ -0,0 +1,11 @@
+﻿namespace MudServer.Common
+{
+	public static class GlobalConst
+	{
+		public const string RoleAdmin = "Admin";
+		public const string RoleCustomer = "Customer";
+		public const string AdminUser1 = "timothyingledue@gmail.com";
+		public const string AdminUser2 = "saingledue@outlook.com";
+		public const string AdminUser3 = "landenfogle@gmail.com";
+	}
+}
---
+++ DigitalShelf/MudServer/Migrations/20240318125550_initialCommit7.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MudServer.Migrations
+{
+    public partial class initialCommit7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
